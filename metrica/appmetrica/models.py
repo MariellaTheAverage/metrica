50f803bc40b7d1a19c4380198e85deb68aa3d710
Mariella <grishenk@petrsu.ru>
2024-04-09 10:35:37 +0300

    set up models and db

--- metrica/appmetrica/models.py
+++ metrica/appmetrica/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 import time
 
+class Product(models.Model):
+    def __str__(self) -> str:
+        return self.str_name
+    
+    str_name = models.CharField(max_length=500)
+
 class Metric(models.Model):
+    def __str__(self) -> str:
+        return f"{self.str_name} in {self.fk_product.str_name}"
+    
+    str_name = models.CharField(max_length=500)
+    fk_product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+
+class MetricValue(models.Model):
+    def __str__(self) -> str:
+        return f"{self.int_timestamp}: {self.int_value}"
+    
     int_timestamp = models.IntegerField(default=int(time.time()))
     int_value = models.IntegerField(default=0)
-
-'''
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-'''
+    fk_metric = models.ForeignKey(Metric, on_delete=models.RESTRICT)
