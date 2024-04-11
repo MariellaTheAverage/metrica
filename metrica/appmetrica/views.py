d49aa8d3c373bddedd845b9816c8e19824be6175
Mariella <grishenk@petrsu.ru>
2024-04-11 17:00:05 +0300

    better views, broke request

--- metrica/appmetrica/views.py
+++ metrica/appmetrica/views.py
@@ -9,17 +9,41 @@ def index(request):
     # print(product_list[0].pk)
     if not product_list:
         raise Http404("List is empty")
-    return render(request, "appmetrica/index.html", {"product_list": product_list})
+    return render(
+        request, 
+        "appmetrica/index.html", 
+        {"product_list": product_list}
+    )
 
 # view all metrics for a specific product
 def view_product(request, product_name):
     if request.POST:
         db_id = request.POST['in-name-prod-id']
         product = get_object_or_404(Product, pk=db_id)
-        return HttpResponse(product)
+        metrics = get_list_or_404(Metric, fk_product=db_id)
+        return render(
+            request,
+            "appmetrica/product.html",
+            {
+                "product": product,
+                "metric_list": metrics
+            }
+        )
+        # return HttpResponse(product)
     else:
         return HttpResponseRedirect(reverse('appmetrica:index'))
 
 # view a graph for a specific metric in a product
-def view_metric(request, product_id, metric_id):
-    return HttpResponse("Here be graph")
+def view_metric(request, product_name, metric_name):
+    if request.POST:
+        pid = request.POST['product-id']
+        mid = request.POST['metric-id']
+        return HttpResponse(f"Metric {mid} ({metric_name}) for product {pid} ({product_name})")
+    return HttpResponse("Empty query")
+
+# get a new metric value from systems (automated or manual)
+def submit_metric(request):
+    if request.POST:
+        # get parameters of metric
+        # insert into db
+        pass
